4d42f8580be141689252e2e9fd18cdaf63896cd4
Kim Jong Seo <whdtj1450@naver.com>
2018-11-15 12:27:58 +0900

    프로젝트 생성 및 폴더 구조변경

---
+++ ThirdMyDiary/ThirdMyDiary/Sources/Shared/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThirdMyDiary
+//
+//  Created by 김종서 on 2018. 11. 15..
+//  Copyright © 2018년 김종서. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
